8e0331afc3ee3d7f7801f042268b7e55b6454837
Jan Reitler <jan.reitler@gmx.de>
2019-09-09 18:39:42 +0200

    Initial commit

---
+++ core/src/main/java/com/reitler/boa/core/App.java
@@ -0,0 +1,13 @@
+package com.reitler.boa.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
