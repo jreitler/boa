9af6b4c1f5e18b94d0f6f9984d34302bab565de4
Jan Reitler <jan.reitler@gmx.de>
2019-12-20 20:33:00 +0100

    create pdf gen project

---
+++ pdfgen/src/main/java/com/reitler/boa/pdfgen/PdfGenerator.java
@@ -0,0 +1,12 @@
+package com.reitler.boa.pdfgen;
+
+import org.apache.fop.apps.FopFactoryBuilder;
+
+import com.reitler.boa.core.interfaces.ISongList;
+
+public class PdfGenerator {
+
+	public void generate(final ISongList list) {
+		new FopFactoryBuilder();
+	}
+}
---
+++ pdfgen/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.reitler.boa.pdfgen {
+
+	requires com.reitler.boa.core.api;
+	requires fop;
+	requires xmlgraphics.commons;
+
+	exports com.reitler.boa.pdfgen;
+}
