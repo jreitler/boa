dd7bab3cbd1558a9e9ab70dfabdb5ac333de16d3
IsabellReitler <50923488+IsabellReitler@users.noreply.github.com>
2019-12-28 21:52:05 +0100

    Change SongChangeDialog

--- app/src/main/java/com/reitler/boa/app/gui/songs/SongChangeDialog.java
+++ app/src/main/java/com/reitler/boa/app/gui/songs/SongChangeDialog.java
@@ -1,14 +1,19 @@
 package com.reitler.boa.app.gui.songs;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Arrays;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.WindowConstants;
 
 import com.reitler.boa.app.gui.UIConstants;
 import com.reitler.boa.core.interfaces.ISong;
@@ -17,13 +22,18 @@ import com.reitler.boa.core.interfaces.factory.SongCreationParameter;
 public class SongChangeDialog extends JDialog {
 
 	private static final long serialVersionUID = -6924059195262743542L;
-	private final JPanel myPanel = new JPanel();
+	private final JPanel centerPanel = new JPanel();
+	private final JPanel buttonPanel = new JPanel();
 	private final GridBagConstraints constraint = new GridBagConstraints();
 
 	private final JTextField title = new JTextField();
 	private final JTextField artist = new JTextField();
 	private final JTextField publisher = new JTextField();
 	private final JTextField tags = new JTextField();
+	private final JButton okButton = new JButton("Ok");
+	private final JButton cancelButton = new JButton("Cancel");
+
+	private SongCreationParameter songParameter;
 
 	public SongChangeDialog(final ISong parameter) {
 		super();
@@ -33,54 +43,99 @@ public class SongChangeDialog extends JDialog {
 			this.publisher.setText(parameter.getPublisher());
 			this.tags.setText(String.join(" ", parameter.getTags()));
 		}
+		showDialog();
 	}
 
 	public SongChangeDialog() {
 		this(null);
 	}
 
-	public SongCreationParameter showDialog() {
-		this.myPanel.setLayout(new GridBagLayout());
+	private void initializeParameter() {
+		this.songParameter = new SongCreationParameter();
+		this.songParameter.title = SongChangeDialog.this.title.getText();
+		this.songParameter.artist = SongChangeDialog.this.artist.getText();
+		this.songParameter.publisher = SongChangeDialog.this.publisher.getText();
+		this.songParameter.tags = Arrays.asList(SongChangeDialog.this.tags.getText().split(" "));
+	}
+
+	public void showDialog() {
+
+		ActionListener listener = new ActionListener() {
+
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				if (e.getSource() == SongChangeDialog.this.okButton) {
+					initializeParameter();
+					setVisible(false);
+				}
+				if (e.getSource() == SongChangeDialog.this.cancelButton) {
+					setVisible(false);
+				}
+
+			}
+		};
+
+		super.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+		this.centerPanel.setLayout(new GridBagLayout());
+		this.buttonPanel.setLayout(new BorderLayout());
 		this.constraint.fill = GridBagConstraints.BOTH;
 
 		this.constraint.weightx = 0.1;
 		this.constraint.gridx = 0;
 		this.constraint.gridy = 0;
 		this.constraint.anchor = GridBagConstraints.WEST;
-		this.myPanel.add(new JLabel(UIConstants.getSongTitleCaption()), this.constraint);
+		this.centerPanel.add(new JLabel(UIConstants.getSongTitleCaption()), this.constraint);
 		this.constraint.gridy = 1;
-		this.myPanel.add(new JLabel(UIConstants.getSongArtistCaption()), this.constraint);
+		this.centerPanel.add(new JLabel(UIConstants.getSongArtistCaption()), this.constraint);
 		this.constraint.gridy = 2;
-		this.myPanel.add(new JLabel(UIConstants.getSongPublisherCaption()), this.constraint);
+		this.centerPanel.add(new JLabel(UIConstants.getSongPublisherCaption()), this.constraint);
 		this.constraint.gridy = 3;
-		this.myPanel.add(new JLabel(UIConstants.getSongTagsCaption()), this.constraint);
+		this.centerPanel.add(new JLabel(UIConstants.getSongTagsCaption()), this.constraint);
 
 		this.constraint.weightx = 1.0;
 		this.constraint.gridx = 1;
 		this.constraint.gridy = 0;
 		this.constraint.anchor = GridBagConstraints.EAST;
-		this.myPanel.add(this.title, this.constraint);
+		this.centerPanel.add(this.title, this.constraint);
 		this.constraint.gridy = 1;
-		this.myPanel.add(this.artist, this.constraint);
+		this.centerPanel.add(this.artist, this.constraint);
 		this.constraint.gridy = 2;
-		this.myPanel.add(this.publisher, this.constraint);
+		this.centerPanel.add(this.publisher, this.constraint);
 		this.constraint.gridy = 3;
-		this.myPanel.add(this.tags, this.constraint);
+		this.centerPanel.add(this.tags, this.constraint);
 
-		int result = JOptionPane.showConfirmDialog(null, this.myPanel, UIConstants.getChangeSongMessage(),
-				JOptionPane.OK_CANCEL_OPTION);
+		super.add(this.centerPanel, BorderLayout.CENTER);
 
-		if (result == JOptionPane.OK_OPTION) {
-			SongCreationParameter songParameter = new SongCreationParameter();
-			songParameter.title = this.title.getText();
-			songParameter.artist = this.artist.getText();
-			songParameter.publisher = this.publisher.getText();
-			songParameter.tags = Arrays.asList(this.tags.getText().split(" "));
-			return songParameter;
-		} else {
-			return null;
-		}
+		this.okButton.addActionListener(listener);
+		this.cancelButton.addActionListener(listener);
 
+		this.buttonPanel.setLayout(new FlowLayout());
+		this.buttonPanel.add(this.okButton);
+		this.buttonPanel.add(this.cancelButton);
+
+		super.add(this.buttonPanel, BorderLayout.SOUTH);
+
+		super.pack();
+
+//		int result = JOptionPane.showConfirmDialog(null, this.centerPanel, UIConstants.getChangeSongMessage(),
+//				JOptionPane.OK_CANCEL_OPTION);
+//
+//		if (result == JOptionPane.OK_OPTION) {
+//			SongCreationParameter songParameter = new SongCreationParameter();
+//			songParameter.title = this.title.getText();
+//			songParameter.artist = this.artist.getText();
+//			songParameter.publisher = this.publisher.getText();
+//			songParameter.tags = Arrays.asList(this.tags.getText().split(" "));
+//			return songParameter;
+//		} else {
+//			return null;
+//		}
+
+	}
+
+	public SongCreationParameter getParameter() {
+		return this.songParameter;
 	}
 
 }
