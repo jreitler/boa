65ce888545192105219af5c0dde98af869e671ec
Jan Reitler <jan.reitler@gmx.de>
2019-12-28 11:11:48 +0100

    Enhance change handling on Songs
    If a setter is called on a Song object, but the new Value is the same as the existing one, no notification is sent.

--- core/src/test/java/com/reitler/boa/core/SongTest.java
+++ core/src/test/java/com/reitler/boa/core/SongTest.java
@@ -2,8 +2,12 @@ package com.reitler.boa.core;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 
 import com.reitler.boa.core.interfaces.ISong;
@@ -94,5 +98,60 @@ public class SongTest {
 		assertEquals("artist", newValueCaptor.getValue().getArtist());
 		assertEquals("newPublisher", newValueCaptor.getValue().getPublisher());
 		assertEquals(1, newValueCaptor.getValue().getId());
+	}
+
+	@Test
+	public void testTagsChangeNotification() {
+		List<String> originalTags = Arrays.asList("tag1", "tag2");
+		Song song = new Song(1);
+		song.setTitle("testSong");
+		song.setArtist("artist");
+		song.setPublisher("publisher");
+		song.setTags(originalTags);
+
+		ISongListener listener = Mockito.mock(ISongListener.class);
+		song.addListener(listener);
+
+		List<String> newTags = Arrays.asList("tag1,", "tag2", "tag3");
+		song.setTags(newTags);
+
+		ArgumentCaptor<ISong> oldValueCaptor = ArgumentCaptor.forClass(ISong.class);
+		ArgumentCaptor<ISong> newValueCaptor = ArgumentCaptor.forClass(ISong.class);
+
+		Mockito.verify(listener, Mockito.times(1)).songChanged(oldValueCaptor.capture(), newValueCaptor.capture());
+
+		assertEquals("testSong", oldValueCaptor.getValue().getTitle());
+		assertEquals("artist", oldValueCaptor.getValue().getArtist());
+		assertEquals("publisher", oldValueCaptor.getValue().getPublisher());
+		assertEquals(originalTags, oldValueCaptor.getValue().getTags());
+		assertEquals(1, oldValueCaptor.getValue().getId());
+
+		assertEquals("testSong", newValueCaptor.getValue().getTitle());
+		assertEquals("artist", newValueCaptor.getValue().getArtist());
+		assertEquals("publisher", newValueCaptor.getValue().getPublisher());
+		assertEquals(newTags, newValueCaptor.getValue().getTags());
+		assertEquals(1, newValueCaptor.getValue().getId());
+	}
+
+	@Test
+	public void testNoNotificationOnNonChange() {
+		List<String> originalTags = Arrays.asList("tag1", "tag2");
+		Song song = new Song(1);
+		song.setTitle("testSong");
+		song.setArtist("artist");
+		song.setPublisher("publisher");
+		song.setTags(originalTags);
+
+		ISongListener listener = Mockito.mock(ISongListener.class);
+		song.addListener(listener);
+
+		// we set the exact same values again after adding the listener...
+		song.setTitle("testSong");
+		song.setArtist("artist");
+		song.setPublisher("publisher");
+		song.setTags(originalTags);
+
+		// ... and expect the listener not to be called, since no value changed
+		Mockito.verify(listener, Mockito.never()).songChanged(ArgumentMatchers.any(), ArgumentMatchers.any());
 	}
 }
